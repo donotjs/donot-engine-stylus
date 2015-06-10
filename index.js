e32acde7330acb20a513d7b531050ee4bae675b7
Kristian Trenskow <trenskow@me.com>
2015-06-10 23:34:52 +0200

    First commit - first revision.

---
+++ index.js
@@ -0,0 +1,15 @@
+var stylus = require('stylus');
+
+exports = module.exports = {
+  map: {
+    '.css': '.styl'
+  },
+  compile: function(file, data, encoding, cb) {
+    var files;
+    source = stylus.render(data, { filename: file }, function(err, source) {
+      if (err) return cb(err);
+      var files = stylus(data).deps(file);
+      cb(null, source, [file].concat(files));
+    });
+  }
+};
---
+++ test/test.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var path = require('path');
+var expect = require('chai').expect;
+var engine = require('../');
+
+var testFile = path.normalize(__dirname + '/data/test.styl');
+var malformedFile = path.normalize(__dirname + '/data/malformed.styl');
+
+describe('stylus', function() {
+
+  var test;
+  var malformed;
+  before(function() {
+    test = fs.readFileSync(testFile, { encoding: 'utf8' });
+    malformed = fs.readFileSync(malformedFile, { encoding: 'utf8' });
+  });
+
+  describe('compiler', function() {
+
+    it ('should return error on malformed stylus', function(done) {
+      engine.compile(malformedFile, malformed, 'utf8', function(err, data) {
+        expect(err).to.be.instanceof(Error);
+        done();
+      });
+    });
+
+    it ('should return css on valid stylus', function(done) {
+      engine.compile(testFile, test, 'utf8', function(err, data) {
+        expect(err).to.be.null;
+        expect(data).to.be.a('string');
+        expect(data).to.be.equal('body {\n  width: 100%;\n}\n');
+        done();
+      });
+    });
+
+  });
+
+});
