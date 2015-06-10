a1c3e391aacc98829ded269d60f7a2437d6e3ecd
Kristian Trenskow <trenskow@me.com>
2015-06-11 00:11:13 +0200

    Made export a function.

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 var stylus = require('stylus');
 
-exports = module.exports = {
-  map: {
-    '.css': '.styl'
-  },
-  compile: function(file, data, encoding, cb) {
-    var files;
-    source = stylus.render(data, { filename: file }, function(err, source) {
-      if (err) return cb(err);
-      var files = stylus(data).deps(file);
-      cb(null, source, [file].concat(files));
-    });
-  }
+exports = module.exports = function() {
+  return {
+    map: {
+      '.css': '.styl'
+    },
+    compile: function(file, data, encoding, cb) {
+      var files;
+      source = stylus.render(data, { filename: file }, function(err, source) {
+        if (err) return cb(err);
+        var files = stylus(data).deps(file);
+        cb(null, source, [file].concat(files));
+      });
+    }
+  };
 };
